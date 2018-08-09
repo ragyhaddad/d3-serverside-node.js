8f0d6727bd00dac78b562e25e6ed0cee3b0ba77c
rhaddad7 <rhaddad7@gatech.edu>
2018-08-09 09:54:18 -0400

    Allow cross-origin

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ var path = require('path');
 var appDir = path.dirname(require.main.filename);
 
 
+/* Allow Cross-Origin request if you want to use your server to do AJAX requests on client-side */
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 /* Set up a public directory to carry your scaffold */
 app.use(express.static('public'))
 
@@ -29,15 +37,16 @@ app.get('/',(req,res) =>{
     //          D3 CODE         //
 
 
-
+    var width = 800;
+    var height = 100;
     /* Create an SVG element and append to the chartContainer */
-    var svg = chartContainer.append('svg').attr('width',1000).attr('height',1000);
-
-    
-
+    var svg = chartContainer.append('svg').attr('width',width).attr('height',height);
 
     //SVG CODE ENDS HERE
+  
+       
 
+    
     /* Send HTTP response carrying your SVG */
     res.send(chartContainer.html());
 
